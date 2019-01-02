1ec2d0852ee02a9cc9ce9bb987b2eceb5a884ad4
黄晨 <18136225579@163.com>
2019-01-03 00:47:54 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  rules: {
+    "linebreak-style": [0 ,"error", "windows"],
+    "indent": ['error', 4]
+  }
+}
